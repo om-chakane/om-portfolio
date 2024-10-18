c49aa7044d1cf0b80eae27de9d73f8c08f4b26f5
OM <omastronaut@gmail.com>
2024-10-18 01:11:01 -0700

    Deploy Portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/om-portfolio',  // Replace 'omchakane' with your repo name
+    images: {
+      unoptimized: true,
+    },
+  };
   
-export default {
-  output: 'export',
-  basePath: '/omchakane',  // Replace 'my-portfolio' with your repo name
-  images: {
-    unoptimized: true,
-  },
-}
+  export default nextConfig; // Export nextConfig instead of a new object
+  
