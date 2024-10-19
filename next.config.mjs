6931d0a41a0a1ae70f14b660431200196b8fd18d
Om Chakane <62010351+om-chakane@users.noreply.github.com>
2024-10-18 17:15:26 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',    // Replace 'omchakane' with your repo name
+    basePath: '/om-portfolio',    // Replace 'omchakane' with your repo name
     images: {
       unoptimized: true,
     },
